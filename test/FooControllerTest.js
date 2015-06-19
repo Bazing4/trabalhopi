95c5f8373a405c77ffd7434eb84bad4b604c4ff4
Marco Antonio <marco2yw@hotmail.com>
2015-06-19 20:09:20 -0300

    esqueleto

---
+++ test/FooControllerTest.js
@@ -0,0 +1,8 @@
+var PessoaController = require("../app/controllers/FooController")
+var assert = require("assert");
+
+describe("FooController", function() {
+	it("FIRST TESTE", function() {
+		assert.equal( true );
+	});
+});
